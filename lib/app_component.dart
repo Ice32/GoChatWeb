64af44ba48e0fe3b4991cdd5c8063f0a1f048ac0
Kenan Selimovic <Kenanselimovic@ministryofprogramming.com>
2018-12-30 12:09:38 +0100

    Move channels listing to new component, ChannelsList

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,28 +1,9 @@
 import 'package:angular/angular.dart';
-import 'package:go_chat/API/ApiSocket.dart';
-import 'package:go_chat/API/ChannelAddEvent.dart';
-import 'package:go_chat/API/ChannelSubscribeMessage.dart';
-import 'package:go_chat/API/EventTypes.dart';
+import 'package:go_chat/channels_list/channels_list.dart';
 
 @Component(
   selector: 'my-app',
-  template: '<div *ngFor="let channel of channels">{{channel}}</div>',
-  providers: [ClassProvider(ApiSocket)],
-  directives: [NgFor]
+  template: '<channels-list></channels-list>',
+  directives: [ChannelsList],
 )
-class AppComponent implements OnInit {
-  ApiSocket _apiSocket;
-  List<String> channels = List<String>();
-
-  AppComponent(this._apiSocket);
-
-  @override
-  void ngOnInit() async {
-    _apiSocket.subscribe(EventTypes.ChannelAdd, this.onChannelAdd);
-    _apiSocket.send(ChannelSubscribeMessage());
-  }
-
-  void onChannelAdd(ChannelAddEvent event) {
-    this.channels.addAll(event.data);
-  }
-}
+class AppComponent {}
---
+++ lib/channels_list/channels_list.dart
@@ -0,0 +1,28 @@
+import 'package:angular/angular.dart';
+import 'package:go_chat/API/ApiSocket.dart';
+import 'package:go_chat/API/ChannelAddEvent.dart';
+import 'package:go_chat/API/ChannelSubscribeMessage.dart';
+import 'package:go_chat/API/EventTypes.dart';
+
+@Component(
+  selector: 'channels-list',
+  templateUrl: 'channels_list.html',
+  providers: [ClassProvider(ApiSocket)],
+  directives: [NgFor]
+)
+class ChannelsList implements OnInit {
+  ApiSocket _apiSocket;
+  List<String> channels = List<String>();
+
+  ChannelsList(this._apiSocket);
+
+  @override
+  void ngOnInit() async {
+    _apiSocket.subscribe(EventTypes.ChannelAdd, this.onChannelAdd);
+    _apiSocket.send(ChannelSubscribeMessage());
+  }
+
+  void onChannelAdd(ChannelAddEvent event) {
+    this.channels.addAll(event.data);
+  }
+}
