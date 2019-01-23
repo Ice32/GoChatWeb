4f59be4c6deb77580ea2f347addd584b5a34b638
Kenan Selimovic <Kenanselimovic@ministryofprogramming.com>
2019-01-23 20:38:31 +0100

    Scroll chat to bottom on load

--- lib/components/messages_container/messages_container.dart
+++ lib/components/messages_container/messages_container.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:angular/angular.dart';
 import 'package:go_chat/components/message_input/message_input.dart';
 import 'package:go_chat/components/messages_list/messages_list.dart';
@@ -8,10 +10,24 @@ import 'package:go_chat/components/messages_list/messages_list.dart';
   styleUrls: const ['./messages_container.css'],
   directives: [MessagesList, MessageInput, NgIf],
 )
-class MessagesContainer{
+class MessagesContainer implements AfterViewChecked{
   @Input("selectedChannel")
   String selectedChannel;
 
   @Input("username")
   String username;
+
+  @ViewChild('messagesListContainer') HtmlElement messagesListContainer;
+
+  ngOnInit() {
+    this._scrollToBottom();
+  }
+
+  ngAfterViewChecked() {
+    this._scrollToBottom();
+  }
+
+  void _scrollToBottom(){
+    this.messagesListContainer.scrollTop = this.messagesListContainer.scrollHeight;
+  }
 }
